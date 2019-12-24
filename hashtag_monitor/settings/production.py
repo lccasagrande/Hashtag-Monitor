5ccfd2865c3025f5eef72927b5369328e9efbd16
lccasagrande <lcamelocasagrande@gmail.com>
2019-12-24 12:14:31 -0300

    (Temp) Add debug to production

--- hashtag_monitor/settings/production.py
+++ hashtag_monitor/settings/production.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hashtag-mon.herokuapp.com']
 LATEST_TWEETS_NB = 100
