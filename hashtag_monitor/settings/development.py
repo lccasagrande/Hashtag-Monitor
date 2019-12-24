44a8845114c7235a72673a67eee18ce93f2cc1e9
lccasagrande <lcamelocasagrande@gmail.com>
2019-12-24 11:58:36 -0300

    Fix collectstatic problem

--- hashtag_monitor/settings/development.py
+++ hashtag_monitor/settings/development.py
@@ -139,7 +139,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
