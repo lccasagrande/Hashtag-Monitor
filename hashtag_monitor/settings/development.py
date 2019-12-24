5aa8484387ac35f13f67741f225add970d3505f0
lccasagrande <lcamelocasagrande@gmail.com>
2019-12-24 12:01:27 -0300

    Del staticfiles dir

--- hashtag_monitor/settings/development.py
+++ hashtag_monitor/settings/development.py
@@ -142,5 +142,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
--- hashtag_monitor/settings/production.py
+++ hashtag_monitor/settings/production.py
@@ -143,7 +143,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
