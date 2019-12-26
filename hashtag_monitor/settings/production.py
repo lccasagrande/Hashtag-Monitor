19618cb4b598aa1771f42ee27ab95d8c47238658
lccasagrande <lcamelocasagrande@gmail.com>
2019-12-26 19:16:07 -0300

    Remove ssl required

--- hashtag_monitor/settings/production.py
+++ hashtag_monitor/settings/production.py
@@ -100,7 +100,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=0)
+db_from_env = dj_database_url.config(conn_max_age=0, ssl_require=False)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
