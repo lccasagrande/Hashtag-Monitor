03f7f0f3b44acf10f5f7bb518d10b19e24306663
lccasagrande <lcamelocasagrande@gmail.com>
2019-12-27 12:27:54 -0300

    Fix setup not correcly installing project

--- setup.py
+++ setup.py
@@ -7,12 +7,18 @@ def read_requirements(filename):
     with open(file) as f:
         return [line.strip() for line in f]
 
+packages = find_packages()
+# Ensure that we don't pollute the global namespace.
+for p in packages:
+    assert p == 'hashtag_monitor' or p.startswith('hashtag_monitor.')
+
 setup(
     name='Hashtag-Monitor',
     version="0.0.1",
     author='lccasagrande',
+    license="MIT",
     package_dir={'hashtag_monitor': 'hashtag_monitor'},
-    packages=find_packages(),
+    packages=packages,
     install_requires=read_requirements('requirements.txt'),
     python_requires='>=3.7',
     scripts=['scripts/manage.py']
